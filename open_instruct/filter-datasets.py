fd4489cec0a642fe18d840a4818ce0609dae2310
jacob-morrison <jacobm@allenai.org>
2023-10-17 15:00:40 -0700

    filter datasets

---
+++ open_instruct/filter-datasets.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+def should_be_filtered(example):
+    # we filter out conversations that contain some specific strings
+    filter_strings = [
+        "OpenAI",
+        "Open AI",
+        "ChatGPT",
+        "Chat GPT",
+        "GPT-3",
+        "GPT3",
+        "GPT 3",
+        "GPT-4",
+        "GPT4",
+        "GPT 4",
+        "GPT-3.5",
+        "GPT3.5",
+        "GPT 3.5",
+        "BingChat",
+        "Bing Chat",
+        "BARD",
+        "Palm",
+        "Anthropic",
+        "Claude",
+        "LAION",
+        "Open Assistant",
+        "OpenAssistant", 
+    ]
+    for message in example["messages"]:
+        if any([filter_string.lower() in message["content"].lower() for filter_string in filter_strings]):
+            return True
+    return False
+
+all_subsets = [f for f in os.listdir(os.path.join('/tulu-data/', "tulu_v2")) if f.endswith("_subset")]
+with open("/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/tulu_v2_data.jsonl", "w") as fout, \
+    open("/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/tulu_v2_filtered_data.jsonl", "w") as fout_filtered:
+    for subset in all_subsets:
+        dataset_name = subset[:-len("_subset")]
+        with open(os.path.join('/tulu-data/', "tulu_v2", subset, f"{dataset_name}_data.jsonl"), "r") as fin, \
+            open(f"/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/{dataset_name}_filtered_data.jsonl", "w") as fout_filtered:
+            for line in fin:
+                example = json.loads(line)
+                if subset not in ["hard_coded_subset"] and should_be_filtered(example):
+                    fout_filtered.write(line)
+                else:
+                    fout.write(line)
--- open_instruct/merge_lora_weights.py
+++ open_instruct/merge_lora_weights.py
@@ -6,3 +6,63 @@
     # 3. save lora weights in new state dicts for each set of lora weights
     # 4. in the new state dict from step # 2, add in merged lora weights to the dictionary (because non-lora weights won't change)
     # 5. re-load base peft model with set_peft_model_state_dict(base_model, new_state_dict)
+
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+import torch
+from peft import LoraConfig, PeftModel, PeftConfig, TaskType, get_peft_model, set_peft_model_state_dict
+from collections import OrderedDict
+import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-c", "--checkpoint", help = "Which checkpoint", type = int)
+# parser.add_argument("-b", "--base_model", help = "Which base model", type = str)
+# parser.add_argument("-l", "--lora", help = "Use lora or not", type = bool)
+# args = parser.parse_args()
+
+
+# load the base model
+base_state_dict = OrderedDict()
+base_model_path = ''
+base_model = AutoModelForCausalLM.from_pretrained(base_model_path)
+tokenizer = AutoTokenizer.from_pretrained(base_model_path)
+base_model.resize_token_embeddings(len(tokenizer))
+
+for key in base_model.state_dict():
+    base_state_dict[key] = base_model.state_dict()[key]
+
+# load the lora modules
+lora_paths = {
+    'orca': ''
+}
+
+lora_state_dicts = {}
+
+for dataset in lora_paths:
+    lora_base_model = AutoModelForCausalLM.from_pretrained(base_model_path)
+    lora_base_model.resize_token_embeddings(len(tokenizer))
+    lora_model = PeftModel.from_pretrained(lora_base_model, lora_paths[dataset])
+    lora_state_dicts[dataset] = OrderedDict()
+    for key in lora_model.state_dict():
+        if key not in base_state_dict:
+            lora_state_dicts[dataset][key] = lora_model.state_dict()[key]
+        else:
+            if lora_model.state_dict()[key].shape == base_state_dict[key].shape:
+                lora_state_dicts[dataset][key] = lora_model.state_dict()[key]
+            else:
+                print('The base shapes don\'t match??')
+                print(lora_model.state_dict()[key].shape)
+                print(base_state_dict[key].shape)
+
+# load a single model to test
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM, 
+    inference_mode=True, 
+    r=256, 
+    lora_alpha=256, 
+    lora_dropout=0.05
+    target_modules=["q_proj", "o_proj", "v_proj", "k_proj", "gate_proj", "up_proj", "down_proj"]
+)
+base_lora_model = AutoModelForCausalLM.from_pretrained(base_model_path)
+base_lora_model.resize_token_embeddings(len(tokenizer))
+model = get_peft_model(base_lora_model, peft_config)
+orca_model = set_peft_model_state_dict(base_model, lora_state_dicts['orca'])
