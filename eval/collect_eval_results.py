15390cc40f8a39aa0d17cdc8716009f90e97620a
jacob-morrison <jacobm@allenai.org>
2023-10-20 11:02:52 -0700

    fix paths

--- eval/collect_eval_results.py
+++ eval/collect_eval_results.py
@@ -13,38 +13,38 @@ for subdir in subdirectories:
     print(dataset)
     # bbh
         # cot
-    with open(os.path.join(subdir, '/bbh/cot/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'bbh/cot/', 'metrics.json')) as f:
         bbh_cot_data = json.loads(f.read())
         # direct
-    with open(os.path.join(subdir, '/bbh/direct/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'bbh/direct/', 'metrics.json')) as f:
         bbh_direct_data = json.loads(f.read())
 
     # gsm
         # cot
-    with open(os.path.join(subdir, '/gsm/cot/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'gsm/cot/', 'metrics.json')) as f:
         gsm_cot_data = json.loads(f.read())
         # direct
-    with open(os.path.join(subdir, '/gsm/direct/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'gsm/direct/', 'metrics.json')) as f:
         gsm_direct_data = json.loads(f.read())
 
     # mmlu-0-shot
-    with open(os.path.join(subdir, '/mmlu-0-shot/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'mmlu-0-shot/', 'metrics.json')) as f:
         mmlu_0_shot_data = json.loads(f.read())
 
     # mmlu-5-shot
-    with open(os.path.join(subdir, '/mmlu-5-shot/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'mmlu-5-shot/', 'metrics.json')) as f:
         mmlu_5_shot_data = json.loads(f.read())
 
     # toxigen
-    with open(os.path.join(subdir, '/toxigen/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'toxigen/', 'metrics.json')) as f:
         toxigen_data = json.loads(f.read())
 
     # tydiqa
         # goldp
-    with open(os.path.join(subdir, '/tydiqa/goldp/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'tydiqa/goldp/', 'metrics.json')) as f:
         tydiqa_goldp_data = json.loads(f.read())
         # no-context
-    with open(os.path.join(subdir, '/tydiqa/no-context/', 'metrics.json')) as f:
+    with open(os.path.join(subdir, 'tydiqa/no-context/', 'metrics.json')) as f:
         tydiqa_no_context_data = json.loads(f.read())
 
     final_data[dataset] = {
