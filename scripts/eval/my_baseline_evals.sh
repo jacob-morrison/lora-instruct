dbfc479f6cef98c8a2e64159e2e6757426eb8674
jacob-morrison <jacobm@allenai.org>
2023-10-20 13:35:43 -0700

    .

--- scripts/eval/my_baseline_evals.sh
+++ scripts/eval/my_baseline_evals.sh
@@ -14,15 +14,15 @@ do
     # MMLU
     for SHOTS in 0 5
     do
-python -m eval.mmlu.run_eval \
-    --ntrain ${SHOTS} \
-    --data_dir /tulu-eval-data/mmlu/ \
-    --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
-    --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-    --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-    --eval_batch_size 2 \
-    --use_chat_format \
-    --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+        python -m eval.mmlu.run_eval \
+            --ntrain ${SHOTS} \
+            --data_dir /tulu-eval-data/mmlu/ \
+            --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
+            --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+            --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+            --eval_batch_size 2 \
+            --use_chat_format \
+            --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
     done
 
     # GSM CoT (WIP)
