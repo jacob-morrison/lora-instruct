24658a1d2a26784c43c29a3773ea19d94711d723
jacob-morrison <jacobm@allenai.org>
2023-10-20 18:23:35 -0700

    run all evals

--- scripts/eval/my_evals.sh
+++ scripts/eval/my_evals.sh
@@ -1,5 +1,5 @@
-# for DATASET in tulu_v2
-for DATASET in code_alpaca_filtered  cot_filtered  flan_v2_filtered  gpt4_alpaca_filtered  hard_coded_filtered  lima_filtered  oasst1_filtered  open_orca_filtered science_filtered  sharegpt_filtered wizardlm_filtered
+# for DATASET in 
+for DATASET in tulu_v2 code_alpaca_filtered  cot_filtered  flan_v2_filtered  gpt4_alpaca_filtered  hard_coded_filtered  lima_filtered  oasst1_filtered  open_orca_filtered science_filtered  sharegpt_filtered wizardlm_filtered
 do
     # toxigen
     python -m eval.toxigen.run_eval \
@@ -12,17 +12,17 @@ do
         --use_vllm \
         --use_chat_format --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
 
-    # # MMLU
-    # python -m eval.mmlu.run_eval \
-    #     --ntrain ${SHOTS} \
-    #     --data_dir /tulu-eval-data/mmlu/ \
-    #     --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
-    #     --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-    #     --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-    #     --lora_weight_path /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/${DATASET}/ \
-    #     --eval_batch_size 2 \
-    #     --use_chat_format \
-    #     --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+    # MMLU
+    python -m eval.mmlu.run_eval \
+        --ntrain ${SHOTS} \
+        --data_dir /tulu-eval-data/mmlu/ \
+        --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
+        --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+        --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+        --lora_weight_path /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/${DATASET}/ \
+        --eval_batch_size 2 \
+        --use_chat_format \
+        --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
 
     # # TruthfulQA (WIP)
     # python -m eval.truthfulqa.run_eval \
