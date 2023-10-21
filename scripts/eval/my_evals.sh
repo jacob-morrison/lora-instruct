5753f252eda1cdbce022deceb42f609ed84b540c
jacob-morrison <jacobm@allenai.org>
2023-10-20 18:24:10 -0700

    fix mmlus

--- scripts/eval/my_evals.sh
+++ scripts/eval/my_evals.sh
@@ -13,16 +13,19 @@ do
         --use_chat_format --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
 
     # MMLU
-    python -m eval.mmlu.run_eval \
-        --ntrain ${SHOTS} \
-        --data_dir /tulu-eval-data/mmlu/ \
-        --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
-        --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-        --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
-        --lora_weight_path /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/${DATASET}/ \
-        --eval_batch_size 2 \
-        --use_chat_format \
-        --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+    for SHOTS in 0 5
+    do
+        python -m eval.mmlu.run_eval \
+            --ntrain ${SHOTS} \
+            --data_dir /tulu-eval-data/mmlu/ \
+            --save_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/results/${DATASET}/mmlu-${SHOTS}-shot \
+            --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+            --tokenizer_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B/ \
+            --lora_weight_path /net/nfs.cirrascale/allennlp/jacobm/tulu_7B_lora_exp/${DATASET}/ \
+            --eval_batch_size 2 \
+            --use_chat_format \
+            --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+    done
 
     # # TruthfulQA (WIP)
     # python -m eval.truthfulqa.run_eval \
