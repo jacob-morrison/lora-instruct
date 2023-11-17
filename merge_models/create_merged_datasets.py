0b932ca8c1e8b74e18b31a8aaa1bc54487cfd30e
jacob-morrison <jacobm@allenai.org>
2023-11-17 14:49:30 -0800

    fix

--- merge_models/create_merged_datasets.py
+++ merge_models/create_merged_datasets.py
@@ -18,8 +18,8 @@ out_dir = '/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/merged_datasets
 file_count = 0
 for i in range(len(datasets) - 1):
     for j in range(i, len(datasets)):
-        file_path1 = f'/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/${datasets[i]}_data.jsonl'
-        file_path2 = f'/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/${datasets[j]}_data.jsonl'
+        file_path1 = f'/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/{datasets[i]}_data.jsonl'
+        file_path2 = f'/net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/{datasets[j]}_data.jsonl'
         with open(file_path1) as f_in1, open(file_path2) as f_in2, open(out_dir + '{datasets[i]}-{datasets[j]}.jsonl') as f_out:
             for line in file_path1:
                 f_out.write(line)
