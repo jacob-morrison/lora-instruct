9876e5daa4e3cf2c09634db9b4a0c75c417e2778
jacob-morrison <jacobm@allenai.org>
2023-11-18 19:10:56 -0800

    train on merged datasets

--- scripts/finetune_lora_with_accelerate.sh
+++ scripts/finetune_lora_with_accelerate.sh
@@ -7,49 +7,49 @@ TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
-# Lora training
-accelerate launch \
-    --mixed_precision bf16 \
-    --num_machines 1 \
-    --num_processes $NUM_GPUS \
-    --use_deepspeed \
-    --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-    open_instruct/finetune.py \
-    --model_name_or_path ../hf_llama2_models/${MODEL_SIZE} \
-    --use_flash_attn \
-    --use_lora \
-    --lora_rank 64 \
-    --lora_alpha 16 \
-    --lora_dropout 0.1 \
-    --tokenizer_name ../hf_llama2_models/${MODEL_SIZE} \
-    --use_slow_tokenizer \
-    --train_file oasst1_data.jsonl \
-    --max_seq_length 4096 \
-    --preprocessing_num_workers 16 \
-    --checkpointing_steps epoch \
-    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-    --learning_rate 1e-4 \
-    --lr_scheduler_type linear \
-    --warmup_ratio 0.03 \
-    --weight_decay 0. \
-    --num_train_epochs 5 \
-    --output_dir output/tulu_v2_${MODEL_SIZE}_lora/ \
-    --with_tracking \
-    --report_to tensorboard \
-    --logging_steps 1 &&
+# # Lora training
+# accelerate launch \
+#     --mixed_precision bf16 \
+#     --num_machines 1 \
+#     --num_processes $NUM_GPUS \
+#     --use_deepspeed \
+#     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+#     open_instruct/finetune.py \
+#     --model_name_or_path ../hf_llama2_models/${MODEL_SIZE} \
+#     --use_flash_attn \
+#     --use_lora \
+#     --lora_rank 64 \
+#     --lora_alpha 16 \
+#     --lora_dropout 0.1 \
+#     --tokenizer_name ../hf_llama2_models/${MODEL_SIZE} \
+#     --use_slow_tokenizer \
+#     --train_file oasst1_data.jsonl \
+#     --max_seq_length 4096 \
+#     --preprocessing_num_workers 16 \
+#     --checkpointing_steps epoch \
+#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+#     --learning_rate 1e-4 \
+#     --lr_scheduler_type linear \
+#     --warmup_ratio 0.03 \
+#     --weight_decay 0. \
+#     --num_train_epochs 5 \
+#     --output_dir output/tulu_v2_${MODEL_SIZE}_lora/ \
+#     --with_tracking \
+#     --report_to tensorboard \
+#     --logging_steps 1 &&
 
-python open_instruct/merge_lora.py \
-    --base_model_name_or_path ../hf_llama2_models/${MODEL_SIZE} \
-    --lora_model_name_or_path output/tulu_v2_${MODEL_SIZE}_lora/ \
-    --output_dir output/tulu_v2_${MODEL_SIZE}_lora_merged/ \
-    --save_tokenizer
+# python open_instruct/merge_lora.py \
+#     --base_model_name_or_path ../hf_llama2_models/${MODEL_SIZE} \
+#     --lora_model_name_or_path output/tulu_v2_${MODEL_SIZE}_lora/ \
+#     --output_dir output/tulu_v2_${MODEL_SIZE}_lora_merged/ \
+#     --save_tokenizer
 
 # my code
 
-BATCH_SIZE_PER_GPU=2
-TOTAL_BATCH_SIZE=128
-GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+# BATCH_SIZE_PER_GPU=2
+# TOTAL_BATCH_SIZE=128
+# GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 
 # DATASET_FILE=flan_v2/flan_v2_data.jsonl \
 # DATASET=flan_v2
@@ -123,7 +123,8 @@ GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 # sharegpt_filtered
 # tulu_v2
 
-for DATASET in hard_coded_filtered oasst1_filtered science_filtered wizardlm_filtered cot_filtered gpt4_alpaca_filtered lima_filtered open_orca_filtered sharegpt_filtered tulu_v2
+# for DATASET in hard_coded_filtered oasst1_filtered science_filtered wizardlm_filtered cot_filtered gpt4_alpaca_filtered lima_filtered open_orca_filtered sharegpt_filtered tulu_v2
+for DATASET in code_alpaca_filtered-code_alpaca_filtered  gpt4_alpaca_filtered-open_orca_filtered   lima_filtered-code_alpaca_filtered    oasst1_filtered-sharegpt_filtered        science_filtered-sharegpt_filtered code_alpaca_filtered-flan_v2_filtered      gpt4_alpaca_filtered-sharegpt_filtered    lima_filtered-flan_v2_filtered        oasst1_filtered-wizardlm_filtered        science_filtered-wizardlm_filtered cot_filtered-code_alpaca_filtered          hard_coded_filtered-code_alpaca_filtered  lima_filtered-lima_filtered           open_orca_filtered-code_alpaca_filtered  sharegpt_filtered-code_alpaca_filtered cot_filtered-cot_filtered                  hard_coded_filtered-cot_filtered          lima_filtered-open_orca_filtered      open_orca_filtered-flan_v2_filtered      sharegpt_filtered-flan_v2_filtered cot_filtered-flan_v2_filtered              hard_coded_filtered-flan_v2_filtered      lima_filtered-sharegpt_filtered       open_orca_filtered-open_orca_filtered    sharegpt_filtered-sharegpt_filtered cot_filtered-gpt4_alpaca_filtered          hard_coded_filtered-gpt4_alpaca_filtered  oasst1_filtered-code_alpaca_filtered  open_orca_filtered-sharegpt_filtered     wizardlm_filtered-code_alpaca_filtered cot_filtered-lima_filtered                 hard_coded_filtered-hard_coded_filtered   oasst1_filtered-cot_filtered          science_filtered-code_alpaca_filtered    wizardlm_filtered-cot_filtered cot_filtered-open_orca_filtered            hard_coded_filtered-lima_filtered         oasst1_filtered-flan_v2_filtered      science_filtered-cot_filtered            wizardlm_filtered-flan_v2_filtered cot_filtered-sharegpt_filtered             hard_coded_filtered-oasst1_filtered       oasst1_filtered-gpt4_alpaca_filtered  science_filtered-flan_v2_filtered        wizardlm_filtered-gpt4_alpaca_filtered gpt4_alpaca_filtered-code_alpaca_filtered  hard_coded_filtered-open_orca_filtered    oasst1_filtered-lima_filtered         science_filtered-gpt4_alpaca_filtered    wizardlm_filtered-lima_filtered gpt4_alpaca_filtered-flan_v2_filtered      hard_coded_filtered-science_filtered      oasst1_filtered-oasst1_filtered       science_filtered-lima_filtered           wizardlm_filtered-open_orca_filtered gpt4_alpaca_filtered-gpt4_alpaca_filtered  hard_coded_filtered-sharegpt_filtered     oasst1_filtered-open_orca_filtered    science_filtered-open_orca_filtered      wizardlm_filtered-sharegpt_filtered gpt4_alpaca_filtered-lima_filtered         hard_coded_filtered-wizardlm_filtered     oasst1_filtered-science_filtered      science_filtered-science_filtered        wizardlm_filtered-wizardlm_filtered
 do
     echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps, on $DATASET using $DATASET_FILE" &&
     accelerate launch \
@@ -140,7 +141,7 @@ do
         --lora_dropout 0.05 \
         --tokenizer_name /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/${MODEL_SIZE} \
         --use_slow_tokenizer \
-        --train_file /net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/${DATASET}_data.jsonl \
+        --train_file /net/nfs.cirrascale/allennlp/jacobm/tulu_data/tulu-v2/merged_datasets/${DATASET}_data.jsonl \
         --max_seq_length 2048 \
         --preprocessing_num_workers 16 \
         --checkpointing_steps epoch \
@@ -151,7 +152,7 @@ do
         --warmup_ratio 0.03 \
         --weight_decay 0. \
         --num_train_epochs 3 \
-        --output_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_${MODEL_SIZE}_lora_exp/${DATASET}/ \
+        --output_dir /net/nfs.cirrascale/allennlp/jacobm/tulu_${MODEL_SIZE}_lora_exp/pairwise_experts/${DATASET}/ \
         --logging_steps 1
 done
 
